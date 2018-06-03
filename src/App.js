dc5be5045d6c1e9f10bb34b3caafd7f42c1addc9
mimanghuilang <jiangmin@hengrong.com>
2018-06-04 00:06:27 +0800

    // 使用react-redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react"
+import {connect} from "react-redux"
+import {addGUN, addGunAsync, removeGUN} from "./index.redux";
+const  mapStatetoProps= (state)=>{
+    return {num:state}
+};
+const actionCreators={addGUN,removeGUN,addGunAsync};
+// App=connect(mapStatetoProps,actionCreators)(App);
+// @connect(state=>({num:state}),{addGUN,removeGUN,addGunAsync})
+@connect(mapStatetoProps,actionCreators)
 class App extends React.Component {
     // constructor(props){
     //     super(props);
     // }
     render() {
-        const {addGUN,removeGUN,store,addGunAsync}=this.props;
-        const num = store.getState();
+        const {addGUN,removeGUN,addGunAsync,num}=this.props;
         return (
             <div>
                 <h1>现在有机枪{num}把</h1>
-                <button onClick={()=>store.dispatch(addGUN())}>申请武器</button>
-                <button onClick={()=>store.dispatch(removeGUN())}>上交武器</button>
-                <button onClick={()=>store.dispatch(addGunAsync())}>拖两天再给</button>
+                <button onClick={addGUN}>申请武器</button>
+                <button onClick={removeGUN}>上交武器</button>
+                <button onClick={addGunAsync}>拖两天再给</button>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import  React from "react"
+import React from "react"
 import ReactDom from "react-dom"
-import {createStore,applyMiddleware,compose} from "redux"
-import thunk  from "redux-thunk"
+import {createStore, applyMiddleware, compose} from "redux"
+import thunk from "redux-thunk"
+import {Provider} from "react-redux"
 import App from "./App";
-import {counter, addGUN, removeGUN, addGunAsync} from "./index.redux"
-const reduxDevtools=window.devToolsExtension;
-const  store=createStore(
+import {counter} from "./index.redux"
+
+const reduxDevtools = window.devToolsExtension;
+const store = createStore(
     counter,
-    compose(applyMiddleware(thunk),reduxDevtools()||{}));
-function  render() {
-    ReactDom.render(<App store={store} addGUN={addGUN} removeGUN={removeGUN} addGunAsync={addGunAsync}/>,document.getElementById("root"))
-}
-render();
-store.subscribe(render);
+    compose(applyMiddleware(thunk), reduxDevtools() || {})
+);
+ReactDom.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById("root")
+);
