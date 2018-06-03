eced8a1365893b49252dc0533b288e36ba9a0a88
mimanghuilang <jiangmin@hengrong.com>
2018-06-03 18:48:22 +0800

    createStore 初体验

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import  React from "react"
-import ReactDom from "react-dom"
-import {createStore} from "redux"
-import App from "./App";
-import  {counter} from "./index.redux"
+// 新建store
+import {createStore} from "redux";
 
-const  store=createStore(counter);
-function  render() {
-    ReactDom.render(<App store={store}/>,document.getElementById("root"))
+function counter(state = 0, action) {
+    switch (action.type) {
+        case "add":
+            return state += 1;
+        case "reduce":
+            return state -= 1;
+        default:
+            return 10;
+    }
 }
-render();
-store.subscribe(render);
+
+const store = createStore(counter);
+const init = store.getState();
+console.log(init);
+
+function listener() {
+    const current = store.getState();
+    console.log(`现在有机枪${current}把`)
+}
+
+store.subscribe(listener);
+store.dispatch({
+    type: "add",
+});
+console.log(store.getState());
+store.dispatch({
+    type: "reduce",
+});
+console.log(store.getState());
