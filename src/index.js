82079cd3a4df482043e854efffbf6d5ed030bb3f
mimanghuilang <jiangmin@hengrong.com>
2018-06-03 18:49:06 +0800

    还原redux的使用

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
-// 新建store
-import {createStore} from "redux";
+import  React from "react"
+import ReactDom from "react-dom"
+import {createStore} from "redux"
+import App from "./App";
+import  {counter} from "./index.redux"
 
-function counter(state = 0, action) {
-    switch (action.type) {
-        case "add":
-            return state += 1;
-        case "reduce":
-            return state -= 1;
-        default:
-            return 10;
-    }
+const  store=createStore(counter);
+function  render() {
+    ReactDom.render(<App store={store}/>,document.getElementById("root"))
 }
-
-const store = createStore(counter);
-const init = store.getState();
-console.log(init);
-
-function listener() {
-    const current = store.getState();
-    console.log(`现在有机枪${current}把`)
-}
-
-store.subscribe(listener);
-store.dispatch({
-    type: "add",
-});
-console.log(store.getState());
-store.dispatch({
-    type: "reduce",
-});
-console.log(store.getState());
+render();
+store.subscribe(render);
