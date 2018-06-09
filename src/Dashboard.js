4a00916445dc0689d451d42c646d4dac824d0a74
mimanghuilang <jiangmin@hengrong.com>
2018-06-09 18:17:07 +0800

    路由的配置下一级

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -19,19 +19,20 @@ class Dashboard extends  React.Component{
         super(props)
     }
     render(){
+        const {match}=this.props;
         const redirectToLogin=<Redirect to="/login"></Redirect>
         const app=(
             <div>
                 <h1>独立团</h1>
                 {this.props.isAuth?<button onClick={this.props.logout}>注销</button>:null}
                 <ul>
-                    <li><Link to="/dashboard">一营</Link></li>
-                    <li><Link to="/dashboard/erying">二营</Link></li>
-                    <li><Link to="/dashboard/sanying">三营</Link></li>
+                    <li><Link to={`${match.url}`}>一营</Link></li>
+                    <li><Link to={`${match.url}/erying`}>二营</Link></li>
+                    <li><Link to={`${match.url}/sanying`}>三营</Link></li>
                 </ul>
-                <Route path="/dashboard/" exact component={App}></Route>
-                <Route path="/dashboard/erying" component={Erying}></Route>
-                <Route path="/dashboard/sanying" component={Sanying}></Route>
+                <Route path={`${match.url}`} exact component={App}></Route>
+                <Route path={`${match.url}/erying`} component={Erying}></Route>
+                <Route path={`${match.url}/sanying`} component={Sanying}></Route>
             </div>
         );
         return this.props.isAuth?app:redirectToLogin;
